8b06a1d0b74e37c67e540697ec5e2f8efd013bce
Mario Fernandez <mario.fernandez@hceris.com>
2012-07-09 23:33:22 +0200

    Settings for chrome

--- keymandorc.rb
+++ keymandorc.rb
@@ -4,8 +4,8 @@ start_at_login(true)
 MessageBoard.change_notifier_to(GrowlNotifier)
 
 ## Global bindings
-map "<Ctrl-Shift-r>" do reload end
-map "<Cmd-Shift-l>" do lock_screen end
+map '<Ctrl-Shift-r>' do reload end
+map '<Cmd-Shift-l>' do lock_screen end
 
 ## Helpers
 
@@ -13,6 +13,13 @@ def unbind(key, *modifiers)
   return if modifiers.empty? or key.size != 1
   seq = "<#{modifiers.join('-')}-#{key}>"
   map seq, nil
+end
+
+def controlify(keys)
+  keys = [keys] if keys.class == String
+  keys.each do |key|
+    map "<Ctrl-#{key}>", "<Cmd-#{key}>"
+  end
 end
 
 ## iTerm
@@ -28,10 +35,24 @@ only /iTerm/ do
   end
 end
 
+## Chrome
+only /Chrome/ do
+  # remap tab selection
+  controlify (1..9).to_a
+
+  # remap some functions to Ctrl-letter
+  controlify ['l', 'n', 't', 'w', 'c', 'v', 'r']
+
+  # Emacs style cancel
+  map '<Ctrl-g>', '<Escape>'
+
+  # Emacs style forward/backward word
+  map '<Cmd-b>', '<Option-Left>'
+  map '<Cmd-f>', '<Option-Right>'
+end
+
 ## Remap bindings in the form Cmd-letter to Ctrl-letter for apps that
 ## don't use emacs-style bindings
-except /iTerm/, /Emacs/, /Quicksilver/, /Xcode/ do
-  ('a'..'z').to_a.each do |key|
-    map "<Ctrl-#{key}>", "<Cmd-#{key}>"
-  end
+except /iTerm/, /Emacs/, /Quicksilver/, /Xcode/, /Chrome/ do
+  controlify ('a'..'z').to_a
 end
