7f3fd6d2402688150b42ef96f7f66f56bc6be153
Mario Fernandez <mario.fernandez@friendscout24.de>
2012-07-09 17:09:07 +0200

    iTerm settings

--- keymandorc.rb
+++ keymandorc.rb
@@ -1,2 +1,21 @@
-start_at_login
+start_at_login(true)
 map "<Ctrl-Shift-R>" { reload }
+
+def unbind(key, *modifiers)
+  return if modifiers.empty? or key.size != 1
+  seq = "<#{modifiers.join('-')}-#{key}>"
+  map seq, nil
+end
+
+## iTerm
+only /iTerm/ do
+  # Don't quit except via command line
+  ['w', 'q', 'r', 't'].each do |key|
+    unbind key, 'Cmd'
+  end
+
+  # Copy/Paste
+  ['c', 'v'].each do |key|
+    map "<Ctrl-Shift-#{key}>", "<Cmd-#{key}>"
+  end
+end
