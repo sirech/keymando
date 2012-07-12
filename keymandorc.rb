edea2941a8057285b3eff18e76576103171c34d4
Mario Fernandez <mario.fernandez@friendscout24.de>
2012-07-12 14:58:08 +0200

    Add select all remappping for chrome

--- keymandorc.rb
+++ keymandorc.rb
@@ -41,7 +41,7 @@ only /Chrome/ do
   controlify (1..9).to_a
 
   # remap some functions to Ctrl-letter
-  controlify ['l', 'n', 't', 'w', 'c', 'v', 'x', 'r']
+  controlify ['a', 'l', 'n', 't', 'w', 'c', 'v', 'x', 'r']
 
   # Emacs style cancel
   map '<Ctrl-g>', '<Escape>'
